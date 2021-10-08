9c77387ab88ad9821db1fde8711289e510f9a3d6
Samson-Miodek <85055886+Samson-Miodek@users.noreply.github.com>
2021-10-08 11:53:06 +0500

    Update BallsForm.cs

--- BallsForm.cs
+++ BallsForm.cs
@@ -1,14 +1,17 @@
-﻿using System;
+using System;
 using System.Drawing;
 using System.Drawing.Drawing2D;
 using System.Windows.Forms;
 using System.Numerics;
+using System.Collections.Generic;
+
 namespace GravityBalls
 {
 	public class BallsForm : Form
 	{
 		private Timer timer;
-		private Ball[] balls;
+		private List<Ball> balls;
+		private Ball radiusBall;
 		private Random random;
 		private int numberBalls = 33;
 
@@ -18,6 +21,7 @@ namespace GravityBalls
 		private int maxVel = 1;
 		private int minVel = -1;
 
+		private bool IsMousePressed;
 		private void UpdWorld()
         {
 			World.height = ClientSize.Height;
@@ -28,6 +32,7 @@ namespace GravityBalls
 		{
 			base.OnResize(e);
 			UpdWorld();
+			radiusBall.radius = World.width * 0.3f;
 		}
 
 		protected override void OnLoad(EventArgs e)
@@ -44,24 +49,33 @@ namespace GravityBalls
 
 			random = new Random();
 
-			balls = new Ball[numberBalls];
+			balls = new List<Ball>();
 
 			for (int i = 0; i < numberBalls; i++)
 			{
-				var x = (float)random.NextDouble() * ClientSize.Width;
-				var y = (float)random.NextDouble() * ClientSize.Height;
-				var r = (float)random.NextDouble() * (maxRadius-minRadius) + minRadius;
+				var x = GetRandomNumber(ClientSize.Width);
+				var y = GetRandomNumber(ClientSize.Height);
+				var r = GetRandomNumber(minRadius,maxRadius);
 				var color = GetColor();
 				var vel = new Vector2(
-					GetRandomNumber(random, minVel, maxVel),
-					GetRandomNumber(random, minVel, maxVel));
-				balls[i] = new Ball(x, y, r,vel,color); 
+					GetRandomNumber(minVel, maxVel),
+					GetRandomNumber(minVel, maxVel));
+				balls.Add(new Ball(x, y, r,vel,color)); 
 			}
+			radiusBall = new Ball(Mouse.x, Mouse.y, World.width * 0.3f, new Vector2(0, 0), new SolidBrush(Color.FromArgb(
+											15,
+											255,
+											255,
+											255)));
 		}
 
-		private static float GetRandomNumber(Random random, int minVel, int maxVel)
+		private float GetRandomNumber(int min, int max)
 		{
-			return (float)random.NextDouble() * maxVel - minVel;
+			return (float)random.NextDouble() * (max - min) + min;
+		}
+		private float GetRandomNumber(int max)
+		{
+			return (float)random.NextDouble() * max;
 		}
 		public SolidBrush GetColor()
         {
@@ -79,23 +93,37 @@ namespace GravityBalls
 		}
 
 		protected override void OnPaint(PaintEventArgs e)
-		{
-			base.OnPaint(e);
-			var g = e.Graphics;
-			g.SmoothingMode = SmoothingMode.HighQuality;
-			for(int i = 0; i < numberBalls; i++)
+        {
+            base.OnPaint(e);
+            var g = e.Graphics;
+            g.SmoothingMode = SmoothingMode.HighQuality;
+
+            //рисует радиус притяжения/отдаления
+            DrawRadiusBall(g);
+
+			if (IsMousePressed)
+				AddBall();
+
+            for (int i = 0; i < balls.Count; i++)
             {
                 var ball = balls[i];
                 ball.Upd();
                 DrawBall(g, ball);
-               // CheckCollisions(i, ball);
+                //CheckCollisions(i, ball);
             }
         }
 
-		//https://ru.wikipedia.org/wiki/%D0%A3%D0%B4%D0%B0%D1%80#%D0%90%D0%B1%D1%81%D0%BE%D0%BB%D1%8E%D1%82%D0%BD%D0%BE_%D1%83%D0%BF%D1%80%D1%83%D0%B3%D0%B8%D0%B9_%D1%83%D0%B4%D0%B0%D1%80_%D0%B2_%D0%BF%D1%80%D0%BE%D1%81%D1%82%D1%80%D0%B0%D0%BD%D1%81%D1%82%D0%B2%D0%B5
-		private void CheckCollisions(int i, Ball ball)
+        private void DrawRadiusBall(Graphics g)
         {
-            for (int j = i + 1; j < numberBalls; j++)
+            radiusBall.pos.X = Mouse.x;
+            radiusBall.pos.Y = Mouse.y;
+            DrawBall(g, radiusBall);
+        }
+
+        //https://ru.wikipedia.org/wiki/%D0%A3%D0%B4%D0%B0%D1%80#%D0%90%D0%B1%D1%81%D0%BE%D0%BB%D1%8E%D1%82%D0%BD%D0%BE_%D1%83%D0%BF%D1%80%D1%83%D0%B3%D0%B8%D0%B9_%D1%83%D0%B4%D0%B0%D1%80_%D0%B2_%D0%BF%D1%80%D0%BE%D1%81%D1%82%D1%80%D0%B0%D0%BD%D1%81%D1%82%D0%B2%D0%B5
+        private void CheckCollisions(int i, Ball ball)
+        {
+            for (int j = i + 1; j < balls.Count; j++)
             {
                 var ball2 = balls[j];
                 var m = ball.radius + ball2.radius;
@@ -140,7 +168,26 @@ namespace GravityBalls
 		protected override void OnMouseClick(MouseEventArgs e)
 		{
 			base.OnMouseMove(e);
-			Ball.K *= -1;
+			if (e.Button == MouseButtons.Left)
+				Ball.K *= -1;
+			if (e.Button == MouseButtons.Right)
+				AddBall();
 		}
-	}
+		protected override void OnMouseDown(MouseEventArgs e)
+		{
+			base.OnMouseMove(e);
+			if (e.Button == MouseButtons.Right)
+				IsMousePressed = true;
+		}
+		protected override void OnMouseUp(MouseEventArgs e)
+		{
+			base.OnMouseMove(e);
+			if (e.Button == MouseButtons.Right)
+				IsMousePressed = false;
+		}
+		private void AddBall()
+        {
+			balls.Add(new Ball(Mouse.x, Mouse.y, GetRandomNumber(minRadius,maxRadius), new Vector2(0, 0), GetColor()));
+        }
+    }
 }
